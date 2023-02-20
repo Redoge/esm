39efa7bd7eedb64ab3e5e8f6caf2fda80ee807ee
Redoge <mail.redoge@gmail.com>
2023-02-21 00:14:30 +0200

    Added WebMvcConfigurer implementation -> MyWebMvcConfigurer

---
+++ src/main/java/com/epam/esm/config/MyWebMvcConfigurer.java
@@ -0,0 +1,23 @@
+package com.epam.esm.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+
+@Configuration
+public class MyWebMvcConfigurer implements WebMvcConfigurer {
+
+    @Override
+    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+        MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter();
+        converter.setSupportedMediaTypes(List.of(MediaType.APPLICATION_JSON));
+        ObjectMapper objectMapper = new ObjectMapper();
+        converter.setObjectMapper(objectMapper);
+        converters.add(converter);
+    }
+}
