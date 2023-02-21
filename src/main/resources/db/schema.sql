b792efe3a8727af9d040e422edcda1b493b7711c
Redoge <mail.redoge@gmail.com>
2023-02-21 14:24:14 +0200

    Added db schema (MySQL dialect)

---
+++ src/main/resources/db/schema.sql
@@ -0,0 +1,24 @@
+CREATE TABLE gift_certificate (
+    gift_certificate_id BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
+    gift_certificate_name VARCHAR(255) NOT NULL,
+    gift_certificate_description VARCHAR(255),
+    gift_certificate_price DECIMAL(10, 2) NOT NULL,
+    gift_certificate_duration INT NOT NULL,
+    gift_certificate_create_date TIMESTAMP NOT NULL,
+    gift_certificate_last_update_date TIMESTAMP NOT NULL,
+    PRIMARY KEY (gift_certificate_id)
+);
+
+CREATE TABLE tag (
+    tag_id BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
+    tag_name VARCHAR(255) NOT NULL,
+    PRIMARY KEY (tag_id)
+);
+
+CREATE TABLE gift_certificate_tag (
+    gift_certificate_id BIGINT UNSIGNED NOT NULL,
+    tag_id BIGINT UNSIGNED NOT NULL,
+    PRIMARY KEY (gift_certificate_id, tag_id),
+    FOREIGN KEY (gift_certificate_id) REFERENCES gift_certificate(gift_certificate_id),
+    FOREIGN KEY (tag_id) REFERENCES tag(tag_id)
+);
