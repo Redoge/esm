2b95aa360d3de9095775c70c19b81a9dd7710ea2
Redoge <mail.redoge@gmail.com>
2023-02-21 13:29:04 +0200

    Added Tag model

---
+++ src/main/java/com/epam/esm/models/Tag.java
@@ -0,0 +1,56 @@
+package com.epam.esm.models;
+
+public class Tag {
+    private long id;
+    private String name;
+
+    public Tag() {
+    }
+
+    public Tag(long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Tag tag = (Tag) o;
+
+        if (id != tag.id) return false;
+        return name.equals(tag.name);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = (int) (id ^ (id >>> 32));
+        result = 31 * result + name.hashCode();
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Tag{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
