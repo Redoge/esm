a1e38edb756ef7159923fc808c807a2b75449739
Redoge <mail.redoge@gmail.com>
2023-03-05 23:59:50 +0200

    added read config from application.yml ability for JdbcConfig

--- src/main/java/com/epam/esm/config/JdbcConfig.java
+++ src/main/java/com/epam/esm/config/JdbcConfig.java
@@ -2,6 +2,8 @@ package com.epam.esm.config;
 
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -10,17 +12,24 @@ import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.sql.DataSource;
+import java.util.Properties;
+
 @Configuration
 
 public class JdbcConfig {
+    private final Properties yamlProperties;
+
+    public JdbcConfig(Properties yamlProperties) {
+        this.yamlProperties = yamlProperties;
+    }
 
     @Bean
     public DataSource dataSource() {
         HikariConfig config = new HikariConfig();
         config.setDriverClassName("com.mysql.cj.jdbc.Driver");
-        config.setJdbcUrl("jdbc:mysql://localhost:3306/esm");
-        config.setUsername("test");
-        config.setPassword("RedogeTest_Password123");
+        config.setJdbcUrl(yamlProperties.getProperty("db.url"));
+        config.setUsername(yamlProperties.getProperty("db.username"));
+        config.setPassword(yamlProperties.getProperty("db.password"));
         config.setMaximumPoolSize(10);
         config.setConnectionTestQuery("SELECT 1");
         return new HikariDataSource(config);
---
+++ src/main/java/com/epam/esm/config/PropertiesReaderConfig.java
@@ -0,0 +1,18 @@
+package com.epam.esm.config;
+
+import org.springframework.beans.factory.config.YamlPropertiesFactoryBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+
+import java.util.Properties;
+
+@Configuration
+public class PropertiesReaderConfig {
+    @Bean
+    public Properties yamlProperties() {
+        YamlPropertiesFactoryBean factory = new YamlPropertiesFactoryBean();
+        factory.setResources(new ClassPathResource("application.yml"));
+        return factory.getObject();
+    }
+}
