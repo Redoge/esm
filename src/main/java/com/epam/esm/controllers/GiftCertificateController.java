38b1cb7b374b0b0aa16ee1ed84f0f065c0c7fb3e
Redoge <mail.redoge@gmail.com>
2023-03-05 19:14:06 +0200

    changed HttpStatusCode.value() to HttpStatus

--- src/main/java/com/epam/esm/controllers/GiftCertificateController.java
+++ src/main/java/com/epam/esm/controllers/GiftCertificateController.java
@@ -2,63 +2,70 @@ package com.epam.esm.controllers;
 
 import com.epam.esm.dto.GiftCertificateMainDto;
 import com.epam.esm.pojo.GiftCertificateSaveRequestPojo;
-import com.epam.esm.services.GiftCertificateService;
+import com.epam.esm.pojo.GiftCertificateSearchRequestPojo;
+import com.epam.esm.services.interfaces.GiftCertificateServiceInterface;
 import com.epam.esm.util.ResponseWrapper;
-import org.springframework.http.HttpStatusCode;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import com.epam.esm.pojo.GiftCertificateSearchRequestPojo;
+
 import java.util.List;
 
 @RestController
 @RequestMapping("api/certificates")
 public class GiftCertificateController {
-    private final GiftCertificateService giftCertificateService;
+    private final GiftCertificateServiceInterface giftCertificateService;
 
-    public GiftCertificateController(GiftCertificateService giftCertificateService) {
+    public GiftCertificateController(GiftCertificateServiceInterface giftCertificateService) {
         this.giftCertificateService = giftCertificateService;
     }
 
-    @GetMapping("")
-    public ResponseEntity<List<GiftCertificateMainDto>> getGiftCertificates(@ModelAttribute GiftCertificateSearchRequestPojo req){
+    @GetMapping
+    public ResponseEntity<List<GiftCertificateMainDto>> getGiftCertificates(@ModelAttribute GiftCertificateSearchRequestPojo req) {
         return ResponseEntity.ok(giftCertificateService.getByGiftCertificateSearchRequestPojo(req));
     }
+
     @GetMapping("/{id}")
-    public ResponseEntity<?> getGiftCertificatesById(@PathVariable long id){
+    public ResponseEntity<?> getGiftCertificatesById(@PathVariable long id) {
         var cert = giftCertificateService.getById(id);
-        return cert.isPresent()? ResponseEntity.ok(cert.get()):
-                new ResponseEntity<>(new ResponseWrapper(404, String.format("Not Found! (id = %d)", id),4042),
-                HttpStatusCode.valueOf(404));
+        return cert.isPresent() ? ResponseEntity.ok(cert.get()) :
+                new ResponseEntity<>(new ResponseWrapper(HttpStatus.NOT_FOUND.value(),
+                        String.format("Not Found! (id = %d)", id), 4042), HttpStatus.NOT_FOUND);
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<?> removeGiftCertificateById(@PathVariable long id){
+    public ResponseEntity<?> removeGiftCertificateById(@PathVariable long id) {
         boolean success = giftCertificateService.deleteById(id);
-        return success?ResponseEntity.ok(new ResponseWrapper(200, String.format("Removed successfully! (id = %d)", id), 2001)) :
-                new ResponseEntity<>(new ResponseWrapper(400, String.format("Deletion is not successful! (id = %d)", id), 4001),
-                        HttpStatusCode.valueOf(400));
+        return success ? ResponseEntity.ok(new ResponseWrapper(HttpStatus.OK.value(),
+                String.format("Removed successfully! (id = %d)", id), 2001)) :
+                new ResponseEntity<>(new ResponseWrapper(HttpStatus.BAD_REQUEST.value(),
+                        String.format("Deletion is not successful! (id = %d)", id), 4001),
+                        HttpStatus.BAD_REQUEST);
     }
-    @PostMapping("")
-    public ResponseEntity<?> create(@RequestBody GiftCertificateSaveRequestPojo giftCert){
+
+    @PostMapping
+    public ResponseEntity<?> create(@RequestBody GiftCertificateSaveRequestPojo giftCert) {
         boolean success;
         success = giftCertificateService.save(giftCert);
-        if(success) giftCert.setId(giftCertificateService.getByName(giftCert.getName()).get().getId());
-        return success? new ResponseEntity<>(new ResponseWrapper(201, String.format("Created successfully! " +
-                "(Name = %s, id = %d)", giftCert.getName(), giftCert.getId()), 2011),HttpStatusCode.valueOf(201))
-                : new ResponseEntity<>(new ResponseWrapper(400, "Created is not successful!", 4002),
-                HttpStatusCode.valueOf(400));
+        if (success) giftCert.setId(giftCertificateService.getByName(giftCert.getName()).get().getId());
+        return success ? new ResponseEntity<>(new ResponseWrapper(HttpStatus.CREATED.value(),
+                String.format("Created successfully! (Name = %s, id = %d)", giftCert.getName(), giftCert.getId()),
+                2011), HttpStatus.CREATED) :
+                new ResponseEntity<>(new ResponseWrapper(HttpStatus.BAD_REQUEST.value(),
+                        "Created is not successful!", 4002),
+                        HttpStatus.BAD_REQUEST);
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<?> update(@PathVariable long id, @RequestBody GiftCertificateSaveRequestPojo giftCert){
-        boolean success = giftCert!=null && (giftCert.getId()==0 || id==giftCert.getId());
-        if(success && giftCert.getId()==0) giftCert.setId(id);
+    public ResponseEntity<?> update(@PathVariable long id, @RequestBody GiftCertificateSaveRequestPojo giftCert) {
+        boolean success = giftCert != null && (giftCert.getId() == 0 || id == giftCert.getId());
+        if (success && giftCert.getId() == 0) giftCert.setId(id);
         success = success && giftCertificateService.update(giftCert, id);
-        return success? new ResponseEntity<>(new ResponseWrapper(200, String.format("Updated successfully! " +
-                "(id = %d)", id), 2011),HttpStatusCode.valueOf(200))
-                : new ResponseEntity<>(new ResponseWrapper(400, String.format("Updated is not successfully! " +
-                "(id = %d)", id), 4002),
-                HttpStatusCode.valueOf(400));
+        return success ? new ResponseEntity<>(new ResponseWrapper(HttpStatus.OK.value(),
+                String.format("Updated successfully! (id = %d)", id), 2011), HttpStatus.OK)
+                : new ResponseEntity<>(new ResponseWrapper(HttpStatus.BAD_REQUEST.value(),
+                String.format("Updated is not successfully! (id = %d)", id), 4002),
+                HttpStatus.BAD_REQUEST);
     }
 
 }
--- src/main/java/com/epam/esm/controllers/TagController.java
+++ src/main/java/com/epam/esm/controllers/TagController.java
@@ -2,9 +2,10 @@ package com.epam.esm.controllers;
 
 import com.epam.esm.dto.TagMainDto;
 import com.epam.esm.models.Tag;
-import com.epam.esm.services.TagService;
+import com.epam.esm.services.interfaces.TagServiceInterface;
 import com.epam.esm.util.ResponseWrapper;
-import org.springframework.http.HttpStatusCode;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,42 +14,46 @@ import java.util.List;
 @RestController
 @RequestMapping("api/tags")
 public class TagController {
-    private final TagService tagService;
+    private final TagServiceInterface tagService;
 
-    public TagController(TagService tagService) {
+    public TagController(TagServiceInterface tagService) {
         this.tagService = tagService;
     }
 
-    @GetMapping("")
-    public ResponseEntity<List<TagMainDto>> getTags(){
+    @GetMapping
+    public ResponseEntity<List<TagMainDto>> getTags() {
         return ResponseEntity.ok(tagService.getAll());
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<?> getTagById(@PathVariable long id){
+    public ResponseEntity<?> getTagById(@PathVariable long id) {
         var tag = tagService.getById(id);
         return tag.isPresent() ? ResponseEntity.ok(tag.get()) :
-                new ResponseEntity<>(new ResponseWrapper(404, String.format("Not Found! (id = %d)", id),4041),
-                HttpStatusCode.valueOf(404));
-    }
-    @DeleteMapping("/{id}")
-    public ResponseEntity<?> removeTagById(@PathVariable long id){
-        boolean success = tagService.deleteById(id);
-        return success ? ResponseEntity.ok(new ResponseWrapper(200, String.format("Removed successfully! (id = %d)", id), 2001)) :
-                new ResponseEntity<>(new ResponseWrapper(400, String.format("Deletion is not successful! (id = %d)", id), 4001),
-                        HttpStatusCode.valueOf(400));
+                new ResponseEntity<>(new ResponseWrapper(HttpStatus.NOT_FOUND.value(),
+                        String.format("Not Found! (id = %d)", id), 4041), HttpStatus.NOT_FOUND);
     }
 
-    @PostMapping("")
-    public ResponseEntity<?> createTag(@RequestBody Tag tag){
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> removeTagById(@PathVariable long id) {
+        boolean success = tagService.deleteById(id);
+        return success ? ResponseEntity.ok(new ResponseWrapper(HttpStatus.OK.value(),
+                String.format("Removed successfully! (id = %d)", id), 2001)) :
+                new ResponseEntity<>(new ResponseWrapper(HttpStatus.BAD_REQUEST.value(),
+                        String.format("Deletion is not successful! (id = %d)", id), 4001),
+                        HttpStatus.BAD_REQUEST);
+    }
+
+    @PostMapping
+    public ResponseEntity<?> createTag(@RequestBody Tag tag) {
         boolean success = false;
-        if(tag.getName()!=null && tag.getName().length()!=0){
+        if (StringUtils.isNotEmpty(tag.getName())) {
             success = tagService.save(tag.getName());
         }
-        if(success) tag.setId(tagService.getByName(tag.getName()).get().getId());
-        return success? new ResponseEntity<>(new ResponseWrapper(201, String.format("Created successfully! " +
-                "(Name = %s, id = %d)", tag.getName(), tag.getId()), 2011),HttpStatusCode.valueOf(201))
-                : new ResponseEntity<>(new ResponseWrapper(400, "Created is not successful!", 4002),
-                HttpStatusCode.valueOf(400));
+        if (success) tag.setId(tagService.getByName(tag.getName()).get().getId());
+        return success ? new ResponseEntity<>(new ResponseWrapper(HttpStatus.CREATED.value(),
+                String.format("Created successfully! (Name = %s, id = %d)", tag.getName(), tag.getId()),
+                2011), HttpStatus.CREATED) :
+                new ResponseEntity<>(new ResponseWrapper(HttpStatus.BAD_REQUEST.value(),
+                        "Created is not successful!", 4002), HttpStatus.BAD_REQUEST);
     }
 }
